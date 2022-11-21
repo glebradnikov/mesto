fc5e89f4af31ff91dc9a4f35e44addc4165d4892
glebradnikov <glebr4dnikov@gmail.com>
2022-11-21 23:48:47 +0300

    refactor: update structure files

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,25 @@
-import Card from './Card.js';
-import FormValidator from './FormValidator.js';
-
-// Объявление переменных
-
-const profileEditButton = document.querySelector('.profile__edit');
-const profileName = document.querySelector('.profile__name');
-const profileWorkplace = document.querySelector('.profile__workplace');
-const profileAddButton = document.querySelector('.profile__add');
-
-const elementsList = document.querySelector('.elements__list');
-
-const popups = document.querySelectorAll('.popup');
-const popupCloseButtons = document.querySelectorAll('.popup__close');
-const popupForm = document.forms;
-const popupForms = document.querySelectorAll('.popup__form');
-const popupInput = document.querySelectorAll('.popup__input');
-const popupError = document.querySelectorAll('.popup__error');
-
-const popupEditProfile = document.querySelector('#popup-edit-profile');
-const formEditProfile = popupForm.editProfile;
-const nameInputEditProfile = formEditProfile.elements.name;
-const workplaceInputEditProfile = formEditProfile.elements.workplace;
-const submitButtonEditProfile = formEditProfile.elements.submitButtonEditProfile;
-
-const popupAddElement = document.querySelector('#popup-add-element');
-const formAddElement = popupForm.addElement;
-const titleInputAddElement = formAddElement.elements.title;
-const urlInputAddElement = formAddElement.elements.url;
-const submitButtonAddElement = formAddElement.elements.submitButtonAddElement;
-
-const popupOpenImage = document.querySelector('#popup-open-image');
-const imageOpenImage = popupOpenImage.querySelector('#image-open-image');
-const imageCaptionOpenImage = popupOpenImage.querySelector('#image-caption-open-image');
-
-// Шесть карточек «из коробки»
-
-const initialElements = [
-  {
-    name: 'Сергиев Посад',
-    link: 'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcT2sYSBCbVPFgNXBYpLGqM-6C6PhmuN8UA8-v1RkkPY_AeN6eEhEoSw34Mni_6ys49g'
-  },
-  {
-    name: 'Переславль-Залесский',
-    link: 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSpJDJF-65H9WAIWtbLyASLQd1gyhDVDRTRoW87xU-zonvstAbHmZaZ2ej5q6Hxv4CC'
-  },
-  {
-    name: 'Ростов',
-    link: 'http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSYXRn1yKNBefFGDE-9frz5ho_CFj71azeriOwyXIUiHoFujCg3DFBOTxqKsdwsMhBJ'
-  },
-  {
-    name: 'Ярославль',
-    link: 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQVHNx0mp2o8d1SqsaygMQRaT9gs_2jOv2UrnXVGQ60rbAyQFCeYGWTgPp81ryLO50y'
-  },
-  {
-    name: 'Кострома',
-    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Ipatiev02.jpg/1280px-Ipatiev02.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQZNA0WIEbiKWwk1wMlQ_4L6Iz0l21P9KlQQsJ2R5mwatNicThwQuadMcX6mA8jtPf2'
-  },
-  {
-    name: 'Владимир',
-    link: 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR3_4lzwSClrnXLzBchtnOLQd5tqqGjPz-IscBlPa8uinSlqxaCSH-7_kJazYGbXJBL'
-  },
-];
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import {
+  profileEditButton,
+  profileName,
+  profileWorkplace,
+  profileAddButton,
+  elementsList,
+  popupCloseButtons,
+  popupEditProfile,
+  formEditProfile,
+  nameInputEditProfile,
+  workplaceInputEditProfile,
+  popupAddElement,
+  formAddElement,
+  titleInputAddElement,
+  urlInputAddElement,
+  popupOpenImage,
+  imageOpenImage,
+  imageCaptionOpenImage,
+  initialElements
+} from '../utils/constants.js';
 
 // Элементы валидации
 
@@ -174,6 +129,8 @@ const openAddElementPopup = () => {
 };
 
 profileAddButton.addEventListener('click', openAddElementPopup);
+
+// Шесть карточек «из коробки»
 
 const createCard = (name, link) => {
   const newCard = new Card(
---
+++ utils/constants.js
@@ -0,0 +1,53 @@
+export const profileEditButton = document.querySelector('.profile__edit');
+export const profileName = document.querySelector('.profile__name');
+export const profileWorkplace = document.querySelector('.profile__workplace');
+export const profileAddButton = document.querySelector('.profile__add');
+
+export const elementsList = document.querySelector('.elements__list');
+
+export const popupCloseButtons = document.querySelectorAll('.popup__close');
+
+export const popupEditProfile = document.querySelector('#popup-edit-profile');
+export const formEditProfile = document.forms.editProfile;
+export const nameInputEditProfile = formEditProfile.elements.name;
+export const workplaceInputEditProfile = formEditProfile.elements.workplace;
+
+export const popupAddElement = document.querySelector('#popup-add-element');
+export const formAddElement = document.forms.addElement;
+export const titleInputAddElement = formAddElement.elements.title;
+export const urlInputAddElement = formAddElement.elements.url;
+
+export const popupOpenImage = document.querySelector('#popup-open-image');
+export const imageOpenImage = popupOpenImage.querySelector('#image-open-image');
+export const imageCaptionOpenImage = popupOpenImage.querySelector('#image-caption-open-image');
+
+export const initialElements = [
+  {
+    name: 'Сергиев Посад',
+    link: 'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcT2sYSBCbVPFgNXBYpLGqM-6C6PhmuN8UA8-v1RkkPY_AeN6eEhEoSw34Mni_6ys49g'
+  },
+  {
+    name: 'Переславль-Залесский',
+    link: 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSpJDJF-65H9WAIWtbLyASLQd1gyhDVDRTRoW87xU-zonvstAbHmZaZ2ej5q6Hxv4CC'
+  },
+  {
+    name: 'Ростов',
+    link: 'http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSYXRn1yKNBefFGDE-9frz5ho_CFj71azeriOwyXIUiHoFujCg3DFBOTxqKsdwsMhBJ'
+  },
+  {
+    name: 'Ярославль',
+    link: 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQVHNx0mp2o8d1SqsaygMQRaT9gs_2jOv2UrnXVGQ60rbAyQFCeYGWTgPp81ryLO50y'
+  },
+  {
+    name: 'Кострома',
+    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Ipatiev02.jpg/1280px-Ipatiev02.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQZNA0WIEbiKWwk1wMlQ_4L6Iz0l21P9KlQQsJ2R5mwatNicThwQuadMcX6mA8jtPf2'
+  },
+  {
+    name: 'Владимир',
+    link: 'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR3_4lzwSClrnXLzBchtnOLQd5tqqGjPz-IscBlPa8uinSlqxaCSH-7_kJazYGbXJBL'
+  },
+];
